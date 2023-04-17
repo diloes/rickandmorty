26561d32f7078b3cbbd789178034cd73abb65ac2
diloes <diloes@hotmail.com>
2023-04-17 19:41:48 +0200

    feat: añadido buscador por nombre

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,39 @@ import Character from './components/Character'
 
 import './App.css'
 
-
 function App() {
-
   const [personajes, setPersonajes] = useState([])
   const [page, setPage] = useState(1)
 
   useEffect(() => {
     const fetchPersonajes = async () => {
-      const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${page}`
+      )
       const { results } = await response.json()
       setPersonajes(results)
       return
     }
 
-      fetchPersonajes()
-  },[page])
+    fetchPersonajes()
+  }, [page])
 
   return (
     <>
       <Routes>
-        <Route path="/" element={ 
-          <Home 
-            personajes={personajes} 
-            page={page}
-            setPage={setPage} 
-          /> } />
-        <Route path="/:id" element={ <Character /> } />
-        <Route path="/*" element={ <h1>Page Not Found</h1> } />
+        <Route
+          path='/'
+          element={
+            <Home
+              personajes={personajes}
+              page={page}
+              setPage={setPage}
+              setPersonajes={setPersonajes}
+            />
+          }
+        />
+        <Route path='/:id' element={<Character />} />
+        <Route path='/*' element={<h1>Page Not Found</h1>} />
       </Routes>
     </>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-export const Home = ({ personajes, page, setPage }) => {
+export const Home = ({ personajes, page, setPage, setPersonajes }) => {
+  const [name, setName] = useState('')
 
   const handlePageNext = () => {
     setPage(page + 1)
@@ -10,33 +12,51 @@ export const Home = ({ personajes, page, setPage }) => {
     setPage(page - 1)
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const response = await fetch(`https://rickandmortyapi.com/api/character?name=${name}`)
+    const { results } = await response.json()
+    setPersonajes(results)
+  }
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
   return (
     <div>
       <div className='header'>
-        <button onClick={handlePagePrev}>{'<<'}</button>
+        <button disabled={page === 1 ? 'disabled' : ''} onClick={handlePagePrev}>
+          {'<<'}
+        </button>
         <h1>Rick & Morty</h1>
         <button onClick={handlePageNext}>{'>>'}</button>
       </div>
-      
-      <div className='container'>
-        {
-          personajes.map(personaje => (
-            <div key={personaje.id}>
-              <Link to={`/${personaje.id}`}>
-                <div className='personaje' >
-                  <img src={personaje.image} alt={personaje.name} />
-                  <h2>{personaje.name}</h2>
-                </div>
-              </Link>
-            </div>
-            
-          ))
-        }
-      </div>
 
+      <form className='search-filter' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Morty Smith'
+          value={name}
+          onChange={handleChange}
+        />
+        <button>Buscar</button>
+      </form>
+
+      <div className='container'>
+        {personajes.map((personaje) => (
+          <div key={personaje.id}>
+            <Link to={`/${personaje.id}`}>
+              <div className='personaje'>
+                <img src={personaje.image} alt={personaje.name} />
+                <h2>{personaje.name}</h2>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   )
-      
 }
 
 export default Home
